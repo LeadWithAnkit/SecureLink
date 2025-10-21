d04b25bc10a58ce3bd35757ad6d57d312a9affe1
Ankit-kTiwari08 <ankitkumartiwari8360@gmail.com>
2025-10-21 18:36:18 +0530

    added additional background for authentication page

--- frontend/src/pages/authentication.jsx
+++ frontend/src/pages/authentication.jsx
@@ -61,7 +61,7 @@ export default function Authentication() {
                     sm={4}
                     md={7}
                     sx={{
-                        backgroundImage: "url('/background.jpg')", 
+                        backgroundImage: "url('/background2.jpg')", 
                         backgroundRepeat: 'no-repeat',
                         backgroundColor: (t) =>
                             t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
