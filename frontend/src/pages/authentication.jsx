5731ee7bd96cf124a4c5e5579701f03ecb190fa8
Ankit-kTiwari08 <ankitkumartiwari8360@gmail.com>
2025-10-21 17:46:55 +0530

    changing landing page

--- frontend/src/pages/authentication.jsx
+++ frontend/src/pages/authentication.jsx
@@ -61,7 +61,7 @@ export default function Authentication() {
                     sm={4}
                     md={7}
                     sx={{
-                        backgroundImage: "url(/background.jpg)",
+                        backgroundImage: "url('/background.jpg')", 
                         backgroundRepeat: 'no-repeat',
                         backgroundColor: (t) =>
                             t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -10,7 +10,8 @@ export default function LandingPage() {
         <div className='landingPageContainer'>
             <nav>
                 <div className='navHeader'>
-                    <h2>Apna Video Call</h2>
+                    <img src="/logo2.png" alt="logo" />
+                    <h1>SecureLink</h1>
                 </div>
                 <div className='navlist'>
                     <p onClick={() => {
@@ -34,7 +35,7 @@ export default function LandingPage() {
                 <div>
                     <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
 
-                    <p>Cover a distance by Apna Video Call</p>
+                    <p>Connecting people. Securing conversations. </p>
                     <div role='button'>
                         <Link to={"/auth"}>Get Started</Link>
                     </div>
