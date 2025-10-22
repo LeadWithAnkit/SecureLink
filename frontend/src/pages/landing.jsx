a2c31a6178d6d4c473abf1d1a234a7cb9d975aed
Ankit-kTiwari08 <ankitkumartiwari8360@gmail.com>
2025-10-22 21:47:54 +0530

    Update landing.jsx

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -35,7 +35,7 @@ export default function LandingPage() {
                 <div>
                     <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
 
-                    <p><b>Connecting people. Securing conversations.</b> </p>
+                    <p><b>Connecting people. Securing conversations .</b> </p>
                     <div role='button'>
                         <Link to={"/auth"}>Get Started</Link>
                     </div>
