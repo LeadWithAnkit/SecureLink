0ed002f3a443bd371a91f15d8da7a8b7e5679a52
Ankit-kTiwari08 <ankitkumartiwari8360@gmail.com>
2025-10-28 20:57:49 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,7 +21,7 @@ app.use(cors({
     "http://localhost:3000",
     "http://localhost:5173",
     "http://127.0.0.1:3000",
-    "https://securelinkfrontend.onrender.com" 
+    "https://securelink-ni4d.onrender.com" 
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
