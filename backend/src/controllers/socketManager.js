d88d60d672d648ce92c21f1b7d2000458fe505b1
Ankit-kTiwari08 <ankitkumartiwari8360@gmail.com>
2025-10-29 22:21:13 +0530

    Update socketManager.js

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -8,7 +8,7 @@ let userRooms = {}
 export const connectToSocket = (server) => {
     const io = new Server(server, {
         cors: {
-            origin: ["http://localhost:3000", "http://localhost:5173", "https://securelinkfrontend.onrender.com"],
+            origin: ["http://localhost:3000", "http://localhost:5173", "https://securelink-ni4d.onrender.com"],
             methods: ["GET", "POST"],
             allowedHeaders: ["*"],
             credentials: true
